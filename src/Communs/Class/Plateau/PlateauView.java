36b3436226610e24f70d892e48d0552b68842c24
Thomas ARROUS <thomas_arrous@yahoo.fr>
2022-12-19 11:16:16 +0100

    Règlage d'un problème j'avais mis les fonction getBoolean et getDirection dans l'interface de la vue de plateau. Maintanant elle sont dans la vue du plateau. Pour facilité leur appel.

--- src/Communs/Class/Plateau/PlateauView.java
+++ src/Communs/Class/Plateau/PlateauView.java
@@ -1,5 +1,7 @@
 package Communs.Class.Plateau;
 
+import java.util.Scanner;
+
 import Communs.Exceptions.positionInvalide;
 import Communs.Interfaces.Carre;
 import Communs.Interfaces.Plateau.InterfacePlateauView;
@@ -102,5 +104,50 @@ public class PlateauView implements Carre, InterfacePlateauView {
             }
         }
         return res;
+    }
+
+    /**
+     * Methode qui permet de demander une direction dans le terminal.
+     * 
+     * @param sc       System.in permettra de lire la reponse de l'utilisateur
+     * @param question La question qui vas être posee.
+     * @return retourn le cote correpondant a la reponse.
+     */
+    public static Direction demandeDirection(Scanner sc, String question) {
+        System.out.println(question);
+        String demande = sc.nextLine();
+        switch (demande.toUpperCase()) {
+            case "DROITE":
+                return Direction.RIGHT;
+            case "GAUCHE":
+                return Direction.LEFT;
+            case "HAUT":
+                return Direction.UP;
+            case "BAS":
+                return Direction.DOWN;
+            default:
+                return Direction.ACTUEL;
+        }
+    }
+
+    /**
+     * Methode qui permet de demander un boolean.
+     * 
+     * @param sc       System.in permettra de lire la reponse de l'utilisateur
+     * @param question La question qui vas être posee.
+     * @return retourn le boolean correpondant a la reponse.
+     */
+    public static boolean demandeBoolean(Scanner sc, String question) {
+        System.out.println(question);
+        String demande = sc.nextLine();
+        switch (demande.toUpperCase()) {
+            case "OUI":
+                return true;
+            case "NON":
+                return false;
+            default:
+                System.out.println("Erreur : réponse invalide.");
+                return demandeBoolean(sc, question);
+        }
     }
 }
--- src/Communs/Interfaces/Plateau/InterfacePlateauView.java
+++ src/Communs/Interfaces/Plateau/InterfacePlateauView.java
@@ -1,9 +1,6 @@
 package Communs.Interfaces.Plateau;
 
-import java.util.Scanner;
-
 import Communs.Class.Plateau.PlateauModel;
-import Communs.Interfaces.Carre.Direction;
 
 /**
  * Interface de la vue du Plateau de jeu.
@@ -19,49 +16,4 @@ public interface InterfacePlateauView {
      * les affiches.
      */
     public String afficher();
-
-    /**
-     * Methode qui permet de demander une direction dans le terminal.
-     * 
-     * @param sc       System.in permettra de lire la reponse de l'utilisateur
-     * @param question La question qui vas être posee.
-     * @return retourn le cote correpondant a la reponse.
-     */
-    public static Direction demandeDirection(Scanner sc, String question) {
-        System.out.println(question);
-        String demande = sc.nextLine();
-        switch (demande.toUpperCase()) {
-            case "DROITE":
-                return Direction.RIGHT;
-            case "GAUCHE":
-                return Direction.LEFT;
-            case "HAUT":
-                return Direction.UP;
-            case "BAS":
-                return Direction.DOWN;
-            default:
-                return Direction.ACTUEL;
-        }
-    }
-
-    /**
-     * Methode qui permet de demander un boolean.
-     * 
-     * @param sc       System.in permettra de lire la reponse de l'utilisateur
-     * @param question La question qui vas être posee.
-     * @return retourn le boolean correpondant a la reponse.
-     */
-    public static boolean demandeBoolean(Scanner sc, String question) {
-        System.out.println(question);
-        String demande = sc.nextLine();
-        switch (demande.toUpperCase()) {
-            case "OUI":
-                return true;
-            case "NON":
-                return false;
-            default:
-                System.out.println("Erreur : réponse invalide.");
-                return demandeBoolean(sc, question);
-        }
-    }
 }
