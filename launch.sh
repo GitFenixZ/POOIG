d00d5613cd001b8c82edb83a6a48eb5b2acba064
GitFenixZ <71153264+GitFenixZ@users.noreply.github.com>
2023-01-02 00:23:59 +0000

    17 fix launch terminal (#18)
    * Creation script de lancement
    
    * deleted images in bin

---
+++ launch.sh
@@ -0,0 +1,21 @@
+compile (){
+
+    echo "Compiling..."
+
+    find -name "*.java" > sources.txt
+    javac -d bin @sources.txt
+    rm sources.txt
+
+    echo "Done!"
+
+}
+
+run (){
+
+    echo "Launching..."
+
+    java -cp bin/ MenuControleur
+
+}
+
+compile && run
